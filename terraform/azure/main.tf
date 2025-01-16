8dd680ab03f277a8a9963fed69e86e93adb7f1e0
si-kobayashi-naoki <naoki.kobayashi@systemi.co.jp>
2025-01-16 11:02:06 +0000

    Falsy commit test

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -19,5 +19,5 @@ resource "azurerm_storage_account" "main" {
 
 resource "azurerm_storage_account_network_rules" "main" {
   storage_account_id = azurerm_storage_account.main.id
-  default_action             = "Deny"
+  default_action             = "Allow"
 }
