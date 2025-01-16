2479baac06663fc9b5a23c88e6ba95d2a35d8f51
si-kobayashi-naoki <naoki.kobayashi@systemi.co.jp>
2025-01-16 09:33:36 +0000

    Default code for trivy scan

---
+++ terraform/azure/main.tf
@@ -0,0 +1,23 @@
+#resource "azurerm_resource_group" "main" {
+#  name     = "main"
+#  location = "Japan East"
+#}
+#
+#resource "azurerm_storage_account" "main" {
+#  name                     = "main"
+#  resource_group_name      = azurerm_resource_group.main.name
+#  location                 = azurerm_resource_group.main.location
+#
+#  account_tier             = "Standard"
+#  account_replication_type = "LRS"
+#
+#  # Security settings
+#  allow_nested_items_to_be_public = false
+#
+#  depends_on = [ azurerm_resource_group.main ]
+#}
+#
+#resource "azurerm_storage_account_network_rules" "main" {
+#  storage_account_id = azurerm_storage_account.main.id
+#  default_action             = "Allow"
+#}
